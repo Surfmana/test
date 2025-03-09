4ef3016c52886770d3021546b11a151451182ef4
lgr <3458943850@qq.com>
2025-03-09 16:02:06 +0800

    this is the second commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("this is the first test!!!");
+        System.out.println("this is the second test!!!");
     }
 }
