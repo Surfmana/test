5cba7ef4d28c01959cfd069aaa3f4170f939a274
lgr <3458943850@qq.com>
2025-03-09 21:28:22 +0800

    test2 first

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("this is the first test!!!");
         System.out.println("this is the second test!!!");
+        System.out.println("test2!!!");
     }
 }
